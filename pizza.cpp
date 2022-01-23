f4dd9a131270faacf7bd5e7adf695681018e6aaa
Tim Kilian <tim@kilian.me>
2022-01-23 11:22:25 +0100

    Add clang-format and format codebase

--- pizza.cpp
+++ pizza.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
-#include <string>
 #include <map>
+#include <string>
 #include <vector>
+
 using namespace std;
+
 map<string, double> vals;
 vector<string> out;
-int main()
-{
-    int n;
-    cin>>n;
-    for(int j=0;j<n;j++){
-        int likes, dislikes;
-        cin>>likes;
-        for(int i=0;i<likes;i++){
-            string local;
-            cin>>local;
-            vals[local]+=(1/(double)likes);
-        }
-        cin>>dislikes;
-        for(int i=0;i<dislikes;i++){
-            string local;
-            cin>>local;
-            vals[local]-=(1/(double)dislikes);
-        }
+
+int main() {
+  int n;
+  cin >> n;
+  for (int j = 0; j < n; j++) {
+    int likes, dislikes;
+    cin >> likes;
+    for (int i = 0; i < likes; i++) {
+      string local;
+      cin >> local;
+      vals[local] += (1 / (double)likes);
     }
-    for(auto it : vals){
-        if(it.second > 0)out.push_back(it.first);
+    cin >> dislikes;
+    for (int i = 0; i < dislikes; i++) {
+      string local;
+      cin >> local;
+      vals[local] -= (1 / (double)dislikes);
     }
-    cout<<out.size()<<" ";
-    for(auto it : out){cout<<it<<" ";}
-    return 0;
+  }
+  for (auto it : vals) {
+    if (it.second > 0)
+      out.push_back(it.first);
+  }
+  cout << out.size() << " ";
+  for (auto it : out) {
+    cout << it << " ";
+  }
+  return 0;
 }
